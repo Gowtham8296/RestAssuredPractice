145b07113a8384600b1d70e04bfff0e1709cc7a8
Gowtham8296 <gowthamgowthu2202@gmail.com>
2024-06-22 16:06:09 +0530

    Creating feature and test java class

---
+++ src/test/java/StepDefFiles/TestStepdef.java
@@ -0,0 +1,14 @@
+package StepDefFiles;
+
+import io.cucumber.java.en.*;
+import javaFiles.APITests;
+
+public class TestStepdef extends APITests {
+    @Given("I m n XYZ page")
+    public void i_m_n_xyz_page() {
+        APITests.driverLauncher();
+//        practing stepdef file
+//        intigarting file
+
+    }
+}
