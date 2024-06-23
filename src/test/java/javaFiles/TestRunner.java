d614c6e70f9023a7517f8ec19c8a4172db3c214f
Gowtham8296 <gowthamgowthu2202@gmail.com>
2024-06-23 21:04:20 +0530

    updated selenium files

--- src/test/java/javaFiles/TestRunner.java
+++ src/test/java/javaFiles/TestRunner.java
@@ -3,7 +3,7 @@ package javaFiles;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions (dryRun = false,glue = {"StepDefFiles","javaFiles","Common"},features = {"src/test/resources/features"},plugin = {"pretty","html:target/cucumber_reports/reports.html","json:target/cucumber_reports/reportjson.json","junit:target/cucumber_reports/xmlreports.xml"})
+@CucumberOptions (dryRun = false,glue = {"StepDefFiles","javaFiles","Common"},features = {"src/test/resources/features"},plugin = {"pretty","html:target/cucumber_reports/reports.html","json:target/cucumber_reports/reportjson.json","junit:target/cucumber_reports/xmlreports.xml"},tags = "@RM")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
--- src/test/resources/features/test.feature
+++ src/test/resources/features/test.feature
@@ -1,4 +1,5 @@
 Feature: xyz
+  @RM
   Scenario: First sample test
     Given I m n XYZ page
     And I wait for driver to quit 5
